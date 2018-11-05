dc918996567fe32105defa66b16538e070785f9a
unknown <peterhu0929@gmail.com>
2018-11-06 01:04:51 +0800

    20181106

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,30 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrinavComponent } from './programs/trinav/trinav.component';
 import { Tri001Component } from './programs/tri001/tri001.component';
 import { Tri002Component } from './programs/tri002/tri002.component';
-import { TrinavComponent } from './programs/trinav/trinav.component';
 
 const routes: Routes =
-  [{ path: 'tri001', component: Tri001Component },
-  { path: 'tri002', component: Tri002Component },
-  { path: 'trinav', component: TrinavComponent }];
+  [{ path: '', redirectTo: '/tri001', pathMatch: 'full' },
+      { path: 'tri001', component: Tri001Component },
+      { path: 'tri002', component: Tri002Component },
+  ];
+  // [
+  //   {
+  //     path: 'home',
+  //     component: TrinavComponent,
+  //     children: [
+  //       { path: 'tri001', component: Tri001Component },
+  //       { path: 'tri002', component: Tri002Component },
+  //       { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //     ]
+  //   },
+  //   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes,
+    { enableTracing: true } )],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
